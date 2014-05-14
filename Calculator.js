d8200899a7604e88f568d8a55f6a448ecd3d5160
unknown <daniel@danielstern.ca>
2014-05-14 16:14:07 -0400

    init

---
+++ Calculator.js
@@ -0,0 +1,339 @@
+function Calculator(config) {
+
+    var calc = this;
+
+    calc.targetAccuracy = 0.0001; // increases the accuracy of the compound interest calculator. the higher it is, the higher you must make precision, which affects performance.
+    calc.calculatorPrecision = 250; // this is an important value as it determines how often you get the "?"
+    calc.allPfsCalculated = [];
+
+
+    var values;
+
+    calc.instructions = function() {
+      console.log("%cThanks for using Calculon!", "color: blue");
+      console.log("%cFunctions", "color: black");
+      console.log("%ccalculator.ror(parameters)", "color: green"); 
+      console.log( "%c@parameters: object { \n}", "color: red");
+
+    }
+
+
+    calc.calculate = function(parameters) {
+
+        var res = undefined;
+
+        var directive;
+
+        if (!parameters) {
+          this.instructions();
+          return;
+        }
+
+        if (!parameters.interestRate) {
+          directive = "interestRate";
+        }
+
+        if (!parameters.startingValue) {
+          directive = "startingValue";
+        }
+
+        if (!parameters.finalValue) {
+          directive = "finalValue";
+        }
+
+        if (!parameters.numMonths) {
+          directive = "numMonths";
+        }
+
+        if (!directive) {
+          console.log("You must pass a valid parameters object with all values but one filled in.");
+          this.instructions();
+          return;
+        }
+
+        switch (directive) {
+            case 'interestRate':
+                res = calc.getInterestRate(parameters);
+                break;
+
+            case ('startingValue'):
+                res = calc.getStartingValue(parameters);
+                break;
+
+            case ('finalValue'):
+                res = calc.getFinalValue(parameters);
+                break;
+
+            case ('numMonths'):
+                res = calc.getNumMonths(parameters);
+                break;
+
+        }
+
+        return res;
+
+
+    }
+
+    calc.ror = calc.calculate;
+
+    calc.getStatistics = function(parameters) {
+
+        var stats = _.clone(parameters);
+
+        stats.values = calc.getChartValues(parameters);
+        stats.startingValue = parameters.startingValue;
+        stats.startingValue = parameters.startingValue;
+
+        return stats;
+
+    }
+
+
+
+
+    calc.refreshStashesValues = function() {
+        calc.allPfsCalculated = [];
+    }
+
+    calc.stashValue = function(value) {
+        calc.allPfsCalculated.push(value);
+    }
+
+    calc.refreshStashesValues();
+
+    calc.getFinalValue = function(values) {
+
+        calc.refreshStashesValues();
+
+        var numMonths = values.numMonths;
+
+        if (values.timeKind == 'yearly') {
+            numMonths *= 12;
+        }
+
+        var p1 = Number(values.startingValue);
+        var pf = p1;
+
+        var pr = Number(values.recurringPayment);
+        var yearlyInterestRate = Number(values.interestRate);
+
+        var monthlyInterestRate = yearlyInterestRate / 12;
+
+        var i = 1 + (monthlyInterestRate / 100);
+
+        if (!p1) p1 = 0;
+        if (!i) i = 1;
+        if (!pr) pr = 0;
+
+        var monthsThru = 0;
+
+        for (var n = 0; n < numMonths; n++) {
+
+            pf += pr;
+            pf = pf * i;
+
+            var stats = {};
+            stats.total = pf;
+            stats.payment = pr;
+            stats.interestPaid = (pf * i) - pf;
+
+            //    calc.stashValue(pf);
+            calc.stashValue(stats);
+
+        }
+
+        return pf.toFixed(2);
+
+    }
+
+    calc.getStartingValue = function(values) {
+
+        var numMonths = values.numMonths;
+
+        var pf = Number(values.finalValue);
+        var p1 = pf;
+
+        var pr = Number(values.recurringPayment);
+        var yearlyInterestRate = Number(values.interestRate);
+
+        var monthlyInterestRate = yearlyInterestRate / 12;
+
+        var i = 1 + (monthlyInterestRate / 100);
+
+        if (!p1) p1 = 0;
+        if (!i) i = 1;
+        if (!pr) pr = 0;
+
+        var monthsThru = 0;
+
+        for (var n = 0; n < numMonths; n++) {
+            if (values.depositFreq == 'monthly') pf -= pr;
+
+            //pf = pf * i;     // for calculating payments at the beginning of the period
+
+            if (values.depositFreq == 'yearly') {
+
+                if (monthsThru == 0) pf -= pr;
+                monthsThru++;
+                if (monthsThru == 12) monthsThru = 0;
+
+            }
+
+            pf = pf / i;
+
+        }
+
+        return pf.toFixed(2);
+
+    }
+
+
+    calc.getInterestRate = function(values) {
+
+        var numMonths = values.numMonths;
+
+
+        var apf = values.finalValue;
+
+        var p1 = Number(values.startingValue);
+        var pf = p1;
+        var pr = Number(values.recurringPayment);
+        var i = 1.01;
+
+        if (pf < p1) return 0;
+
+        var monthsThru = 0;
+
+        var guessI = i;
+
+        var precision;
+        var targetPrecision = 10; // less than this amount apart  
+
+        var count = calc.calculatorPrecision;
+        var adjustmentAmount = 0.01;
+
+        while (count > 0) {
+            calc.allPfsCalculated = [];
+
+            for (var n = 0; n < numMonths; n++) {
+                if (values.depositFreq == 'monthly') pf += pr;
+
+                if (values.depositFreq == 'yearly') {
+
+                    if (monthsThru == 0) pf += pr;
+                    monthsThru++;
+                    if (monthsThru == 12) monthsThru = 0
+
+                }
+
+                pf = pf * guessI;
+                calc.allPfsCalculated.push(pf);
+
+            }
+
+            if (apf > pf) {
+
+                guessI += adjustmentAmount;
+
+            } else {
+
+                guessI -= adjustmentAmount;
+
+            }
+            precision = Math.abs(pf - apf);
+
+            if (precision < calc.targetAccuracy) {
+                break;
+            }
+
+
+            adjustmentAmount *= 0.9;
+            pf = p1;
+
+            count--;
+        }
+
+        i = ((guessI - 1) * 12) * 100;
+
+        if (precision > 10 || isNaN(precision || count >= calc.calculatorPrecision)) return "?"
+        $('#interestError').html('');
+        return i.toFixed(3);
+
+    }
+
+    calc.getChartValues = function(parameters) {
+        calc.getFinalValue(parameters);
+        return calc.allPfsCalculated;
+    }
+
+    calc.getNumMonths = function(values) {
+
+        var apf = values.finalValue;
+        var p1 = Number(values.startingValue);
+        var pf = p1;
+        var pr = Number(values.recurringPayment);
+        var i = Number(values.interestRate) / 12;
+
+        var monthsThru = 0;
+
+        var guessNumMonths = 120;
+
+        var precision;
+        var targetPrecision = 25; // less than this amount apart  
+
+        var timeTargetAccuracy = 50;
+
+        var count = calc.calculatorPrecision;
+        var adjustmentAmount = 15;
+
+        while (count > 0) {
+            calc.allPfsCalculated = [];
+            var numMonths = guessNumMonths;
+
+            for (var n = 0; n < numMonths; n++) {
+                if (values.depositFreq == 'monthly') pf += pr;
+
+                if (values.depositFreq == 'yearly') {
+
+                    if (monthsThru == 0) pf += pr;
+                    monthsThru++;
+                    if (monthsThru == 12) monthsThru = 0
+
+                }
+
+                pf = pf * (1 + (i / 100));
+
+                calc.allPfsCalculated.push(pf);
+
+            }
+
+            if (apf > pf) {
+
+                guessNumMonths += adjustmentAmount;
+
+            } else {
+
+                guessNumMonths -= adjustmentAmount;
+
+            }
+
+            precision = Math.abs(pf - apf);
+
+            if (precision < timeTargetAccuracy) break;
+
+            adjustmentAmount *= 0.95;
+            pf = p1;
+
+            count--;
+        }
+
+        var temporalAccuracyTarget = pf * 0.22;
+
+        if (isNaN(precision) || precision > temporalAccuracyTarget) return "?"
+        return numMonths.toFixed(3);
+
+    }
+}
+
+Calculator.instructions = Calculator.prototype.instructions;
